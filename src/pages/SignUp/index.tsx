dfde9c932700ca342c556aa2578ca1f630a92740
boksooni <8545652@naver.com>
2022-09-26 20:07:02 +0900

    💄 design: 변수명, 디자인 수정/#79

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,41 +4,53 @@ import { SignUpIcon } from 'components/Icons'
 
 export const SignUp = () => {
   return (
-    <S.SignUpContainer>
-      <S.SignUpWrapper>
-        <S.SignUpText>회원가입</S.SignUpText>
-        <S.SignUpEmailWrapper>
-          <S.SignUpText2>이메일</S.SignUpText2>
+    <S.Container>
+      <S.Wrapper>
+        <S.TopText>회원가입</S.TopText>
+        <S.EmailWrapper>
+          <S.MidText>이메일</S.MidText>
           <S.PositionWrap>
-            <S.SignUpInput type='text' />
+            <S.EmailInput
+              type='text'
+              placeholder='이메일 주소를 입력해 주세요.'
+              required
+            />
             <S.IdCheckBtn>중복 확인</S.IdCheckBtn>
           </S.PositionWrap>
           <S.TempSpan>(이메일 중복 멘트)</S.TempSpan>
-        </S.SignUpEmailWrapper>
-        <S.SignUpEmailWrapper>
-          <S.SignUpText2>비밀번호</S.SignUpText2>
+        </S.EmailWrapper>
+        <S.PwWrapper>
+          <S.MidText>비밀번호</S.MidText>
           <S.PositionWrap>
-            <S.SignUpPwInput type='paswword' />
-            <S.PasswordCheckBtn>
+            <S.PwInput
+              type='paswword'
+              placeholder='비밀번호를 입력해 주세요.'
+              required
+            />
+            <S.PwCheckBtn>
               <SignUpIcon width='18' height='18' />
-            </S.PasswordCheckBtn>
+            </S.PwCheckBtn>
           </S.PositionWrap>
           <S.TempSpan>
             * 영문 대소문자, 숫자, 특수문자 중 2가지 이상 조합(8~50자)
           </S.TempSpan>
-        </S.SignUpEmailWrapper>
-        <S.SignUpEmailWrapper>
-          <S.SignUpText2>비밀번호 확인</S.SignUpText2>
+        </S.PwWrapper>
+        <S.PwWrapper>
+          <S.MidText>비밀번호 확인</S.MidText>
           <S.PositionWrap>
-            <S.SignUpPwInput type='password' />
-            <S.PasswordCheckBtn>
+            <S.PwInput
+              type='password'
+              placeholder='비밀번호를 확인해 주세요.'
+              required
+            />
+            <S.PwCheckBtn>
               <SignUpIcon width='18' height='18' />
-            </S.PasswordCheckBtn>
+            </S.PwCheckBtn>
           </S.PositionWrap>
           <S.TempSpan>(비밀번호 중복 확인 멘트)</S.TempSpan>
-        </S.SignUpEmailWrapper>
-        <S.SignUpBtn>회원가입하기 </S.SignUpBtn>
-      </S.SignUpWrapper>
-    </S.SignUpContainer>
+        </S.PwWrapper>
+        <S.SignUpBtn>회원가입</S.SignUpBtn>
+      </S.Wrapper>
+    </S.Container>
   )
 }
